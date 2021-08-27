30908b16f92e6a1e7f625bed1e0ba7484b0ddf04
natalibogarin <natali.bogarin@gmail.com>
2021-08-27 14:51:24 -0300

    Heroku config upd

--- django_site/settings/base.py
+++ django_site/settings/base.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'apps.blog',
     'apps.portfolio'
 ]
