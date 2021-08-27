9dfc829a470a4456d1fbb2822d034ccf56d34180
natalibogarin <natali.bogarin@gmail.com>
2021-08-26 22:18:27 -0300

    Heroku config added

--- django_site/settings/production.py
+++ django_site/settings/production.py
@@ -2,6 +2,7 @@
 from base import *
 
 import dj_database_url
+import django_heroku
 
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
@@ -19,6 +20,8 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
+
 
 #STATIC_URL = 'https://nombreurlproyecto.com/static/'
 #MEDIA_URL = 'https://nombreurlproyecto.com/media/'
